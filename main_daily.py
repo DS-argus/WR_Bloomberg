add165f5744585505d624ea5383785b9adfdd674
quantwoori <sangil.bae@wooriam.kr>
2022-06-03 09:50:17 +0900

    Delete excel update function
    find a way to get data directly from MSSQL

--- main_daily.py
+++ main_daily.py
@@ -30,87 +30,6 @@ def get_token(target:str) -> str:
     return dat['sql'][target]
 
 
-def get_hist_data_from_sql(from_: date,
-                           to_: date,
-                           idxs: list,
-                           type: str = "o",
-                           ffill: bool = True) -> pd.DataFrame:
-
-    server = MSSQL.instance()
-    server.login(
-        id=get_token('id'),
-        pw=get_token('pw')
-    )
-
-    # 시작일이 공휴일이면 ffill 할 수가 없어 넉넉하게 과거 10일 데이터 불러와서 출력은 시작일부터
-    # DB에 2000/01/04 부터 들어가 있어 CSI300은 처음엔 NaN으로 나옴
-    if from_ < date(2000, 1, 4):
-        from_ = date(2000, 1, 4)
-
-    from_ = from_ - timedelta(10)
-
-    # query 조건
-    cond_name = [f"NAME = '{idx}'" for idx in idxs]
-    cond_name = " or ".join(cond_name)
-
-    cond = [
-        f"DATE >= '{from_.strftime('%Y%m%d')}'",
-        f"DATE <= '{to_.strftime('%Y%m%d')}'",
-        f"({cond_name})"
-    ]
-
-    cond = ' and '.join(cond)
-
-    # data 불러오기
-    col = ['DATE', 'NAME', 'TICKER', 'TYPE', 'VALUE']
-    d = server.select_db(
-        database='WSOL',
-        schema='dbo',
-        table='drvprc',
-        column=col,
-        condition=cond
-    )
-
-    # data 가공
-    d = pd.DataFrame(d)
-    d = pd.pivot_table(d, values=d.columns[4], index=d.columns[0], columns=d.columns[1])
-    d.index = pd.Series([datetime.strptime(day, "%Y%m%d") for day in d.index])
-    d.index.name = "Date"
-
-    # DB에서 받아온 index는 datetime 형태, date만 있는 object 타입으로 바꿔줌(class_els와의 호환 위해)
-    d.index = d.index.date
-
-    # type = 'w'인 경우도 db에 있는 날까지 나오도록
-    to_ = d.index[-1]
-
-    # 공휴일, 휴일제외 original version
-    if type == "o":
-        if ffill is True:
-            d.fillna(method='ffill', inplace=True)
-            return d.iloc[10:, :]
-        else:
-            return d.iloc[10:, :]
-
-    # 모든 날 추가한 version
-    elif type == "w":
-        dt_rng = pd.date_range(from_, to_).date
-        d_weekend = pd.DataFrame(index=dt_rng, columns=idxs)
-
-        for day in d.index:
-            try:
-                d_weekend.loc[day] = d.loc[day]
-
-            except KeyError as e:
-                d_weekend.loc[day] = [np.nan] * len(idxs)
-
-        if ffill is True:
-            d_weekend.fillna(method='ffill', inplace=True)
-
-            return d_weekend.iloc[10:, :]
-        else:
-            return d_weekend.iloc[10:, :]
-
-
 class InsertData:
     def __init__(self, work:str='initial'):
         """
@@ -228,43 +147,8 @@ class InsertData:
                 duples += 1
                 continue
 
-def Create_excel():
-
-    def get_data() -> pd.DataFrame:
-        start = date(2018, 1, 1)
-        end = date.today() - timedelta(days=1)
-
-        underlyings = [
-            'KOSPI200', 'HSCEI', 'HSI', 'NIKKEI225', 'S&P500', 'EUROSTOXX50', 'CSI300',
-            'S&P500(Q)', 'EUROSTOXX50(Q)',
-            'S&P500(KRW)', 'EUROSTOXX50(KRW)', 'HSCEI(KRW)'
-        ]
-
-        df = get_hist_data_from_sql(start, end, underlyings, type='w', ffill=True)
-
-        return df
-
-    def update_excel(dataframe: pd.DataFrame):
-        with xw.App(visible=False)as app:
-            ex = xw.Book(r'\\172.31.1.222\GlobalD\Derivatives\DB 종가데이터\DB 종가데이터.xlsx')
-            sh1 = ex.sheets['DB종가']
-
-            sh1.range("A1:M8000").clear_contents()
-
-            sh1[0, 0].options(index=True).value = dataframe
-
-            ex.save()
-            ex.close()
-
-    df = get_data()
-    update_excel(df)
-
-    return
-
 
 if __name__ == '__main__':
     # 넉넉하게 5일전부터 어제까지의 데이터 받는 걸로 daily 설정. 중복PK는 알아서 걸러지니깐
     indat = InsertData(work='daily')
     indat.run()
-
-    Create_excel()
